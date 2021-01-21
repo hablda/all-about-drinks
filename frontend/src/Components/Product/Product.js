3d210c75eeb108a1a1ef1655b1916e4295785c67
David Habl <habl@redblue.de>
2021-01-21 17:29:26 +0100

    feat: check if product data is available for product component

--- frontend/src/Components/Product/Product.js
+++ frontend/src/Components/Product/Product.js
@@ -2,16 +2,17 @@ import React from 'react';
 import StyledProduct from './Product.styled';
 import { Heading } from '../Typo/Heading';
 import { Picture } from '../Picture';
+import { Price } from '../Price';
 
 export const Product = ({ product }) => {
   const { image, number, name, description, price } = product;
   return (
     <StyledProduct>
-      <Picture image={image} round fullwidth />
-      <p>Article No.: {number}</p>
-      <Heading tag="h2">{name}</Heading>
-      <p>{description}</p>
-      <p>{price}</p>
+      {image ? <Picture image={image} round fullwidth /> : null}
+      {number ? <p>Article No.: {number}</p> : null}
+      {name ? <Heading tag="h2">{name}</Heading> : null}
+      {description ? <p>{description}</p> : null}
+      {price ? <Price price={price}></Price> : null}
     </StyledProduct>
   );
 };
