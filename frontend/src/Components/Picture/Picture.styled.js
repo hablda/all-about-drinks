3d2411f756ae9f4658d17cdda638e43d0ea02c67
David Habl <habl@redblue.de>
2021-02-22 14:53:39 +0100

    fix: style typo

--- frontend/src/Components/Picture/Picture.styled.js
+++ frontend/src/Components/Picture/Picture.styled.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 const StyledPictureContainer = styled.div(({ round, fullwidth, theme }) => {
   const { spacing } = theme;
   return css`
-    border-radiusssss: ${round ? spacing.m : null};
+    border-radius: ${round ? spacing.m : null};
     overflow: hidden;
 
     img {
